9e590e5b2693556d83b5460d21f0bf2dde903176
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-09-21 03:16:27 +0100

    added github connection

---
+++ app.js
@@ -0,0 +1,19 @@
+// init github
+const github = new GitHub;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// search input event listener
+searchUser.addEventListener('keyup', (e) =>{
+    // get input text
+    const userText = e.target.value;
+
+    if(userText !== ''){
+        // make http call
+        github.getUser(userText)
+        .then(data => {
+           console.log(data);
+        })
+    }
+});
